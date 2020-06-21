45500f5c7276c58e2634a6eb85a092063666913d
CSolutions4 <67233144+CSolutions4@users.noreply.github.com>
2020-06-21 23:29:35 +0000

    Update StorageAzure.cs

--- NETCoreWebAPISQLServerAzure/Controllers/StorageAzure.cs
+++ NETCoreWebAPISQLServerAzure/Controllers/StorageAzure.cs
@@ -7,12 +7,12 @@ namespace NETCoreWebAPISQLServerAzure.Controllers
 {
     public class StorageAzure
     {
-        public List<perros> ListadePerros;
-        public bool Almacenar(string Nombre, string Sexo, int year_adopcion, string esterilizacion, string direccion)
+        public List<usuarios> ListadeUsuarios;
+        public bool Almacenar(int CURP, string Nombre, string Sexo, int Edad, string Nscimiento, string registro, string direccion)
         {
             var connect = new SqlConnection
-                ("Server=tcp:perros.database.windows.net,1433;" +
-                "Initial Catalog=App_Perror;" +
+                ("Server=tcp:usuarios.database.windows.net,1433;" +
+                "Initial Catalog=App_User;" +
                 "Persist Security Info=False;" +
                 "User ID=erikroot;" +
                 "Password=6TeQ&KeraN16;" +
@@ -21,8 +21,8 @@ namespace NETCoreWebAPISQLServerAzure.Controllers
                 "TrustServerCertificate=False;" +
                 "Connection Timeout=30;");
             var query = new SqlCommand
-                ("INSERT INTO Datos (Nombre, Sexo, year_adopcion, esterilizacion, direccion) VALUES " +
-                "('" + Nombre + "','" + Sexo + "','" + year_adopcion + "', '" + esterilizacion + "','" + direccion + "')", connect);
+                ("INSERT INTO Datos (CURP, Nombre, Sexo, Edad, Nacimiento, Registro, Direccion) VALUES " +
+                "('" + CURP + "','" + Nombre + "','" + Sexo + "','" + Edad + "', '" + Nacimiento + "', '" + Registro + "','" + Direccion + "')", connect);
 
             try
                 {
@@ -39,12 +39,12 @@ namespace NETCoreWebAPISQLServerAzure.Controllers
             }
         }
 
-        public List<perros> Consulta(int ID)
+        public List<Usuarios> Consulta(int CURP)
         {
             var dt = new DataTable();
             var connect = new SqlConnection
-                ("Server=tcp:perros.database.windows.net,1433;" +
-                "Initial Catalog=App_Perror;" +
+                ("Server=tcp:usuarios.database.windows.net,1433;" +
+                "Initial Catalog=App_users;" +
                 "Persist Security Info=False;" +
                 "User ID=erikroot;" +
                 "Password=6TeQ&KeraN16;" +
@@ -53,21 +53,22 @@ namespace NETCoreWebAPISQLServerAzure.Controllers
                 "TrustServerCertificate=False;" +
                 "Connection Timeout=30;");
             var cmd = new SqlCommand
-                ("SELECT * From Datos WHERE ID ='" + ID + "'", connect);
+                ("SELECT * From Datos WHERE ID ='" + CURP + "'", connect);
             connect.Open();
             var da = new SqlDataAdapter(cmd);
             da.Fill(dt);
-            ListadePerros = new List<perros>();
-            perros perro = new perros();
-            perro.ID = int.Parse((dt.Rows[0]["ID"]).ToString());
-            perro.Nombre = (dt.Rows[0]["Nombre"]).ToString();
-            perro.Sexo = (dt.Rows[0]["Sexo"]).ToString();
-            perro.year_adopcion = int.Parse((dt.Rows[0]["year_adopcion"]).ToString());
-            perro.esterilizacion = (dt.Rows[0]["esterilizacion"]).ToString();
-            perro.direccion = (dt.Rows[0]["direccion"]).ToString();
-            ListadePerros.Add(perro);
+            Listadeusuarios = new List<Usuarios>();
+            Usuarios usuario = new usuarios();
+            Usuario.CURP = int.Parse((dt.Rows[0]["CURP"]).ToString());
+            Usuario.Nombre = (dt.Rows[0]["Nombre"]).ToString();
+            Usuario.Sexo = (dt.Rows[0]["Sexo"]).ToString();
+            Usuario.Edad = int.Parse((dt.Rows[0]["Edad"]).ToString());
+            Usuario.Nacimiento = (dt.Rows[0]["Nacimiento"]).ToString();
+            Usuario.Registro = (dt.Rows[0]["Registro"]).ToString();
+            Usuario.Direccion = (dt.Rows[0]["direccion"]).ToString();
+            ListadeUsuarios.Add(user);
 
-            return ListadePerros;
+            return ListadeUsuarios;
         }
     }
 }
