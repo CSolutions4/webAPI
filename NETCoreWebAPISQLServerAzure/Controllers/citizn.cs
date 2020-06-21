49762dfbb28418ee85ee9cc4860a6267d3e8b1ae
CSolutions4 <67233144+CSolutions4@users.noreply.github.com>
2020-06-21 20:09:16 +0000

    Update and rename perros.cs to citizn.cs

---
+++ NETCoreWebAPISQLServerAzure/Controllers/citizn.cs
@@ -0,0 +1,13 @@
+﻿namespace NETCoreWebAPISQLServerAzure.Controllers
+{
+    public class citizen
+    {
+        public int CURP { get; set; }
+        public string Nombre { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public string Fecha_nacimiento { get; set; }
+        public string Fecha_registro { get; set; }
+        public string Direccion { get; set; }
+    }
+}
